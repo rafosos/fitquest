7ba9bf0fc5a21058423d5a0edc258bf0aa0b236a
rafosos <rafael.lrfrancisco@gmail.com>
2025-04-15 15:49:40 -0300

    mostrar no erro 400 a mesnagem que veio do backend

--- frontend/utils/ErrorHandler.ts
+++ frontend/utils/ErrorHandler.ts
@@ -13,25 +13,33 @@ export const ErrorHandler = () => {
         if(isAxiosError(error)){
             error as AxiosError;
 
+            let tipo = {type: "danger"};
+
             if(error.response){
+
                 if (error.response.status == 401){
                     signOut();
-                    toast.show("Você precisa logar novamente!", {type: 'danger'});
+                    toast.show("Você precisa logar novamente!", tipo);
                     return
+                }
+
+                if (error.response.status == 400 && error.response.data.detail){
+                    toast.show(error.response.data.detail, tipo);
+                    return;
                 }
 
                 // The request was made and the server responded with a status code
                 // that falls out of the range of 2xx
                 console.log("error.response:  #############################################################################")
-                console.log(error.response.data);
                 console.log(error.response.status);
+                console.log(error.response.data);
                 console.log(error.response.headers);
             }
             if (error.request) {
                 console.log("error.request:   #########################################################################")
                 console.log(error.request);
             }
-            toast.show("Erro: " + error.message, {type: 'danger'});
+            toast.show("Ops, ocorreu um erro :(\nPor favor, tente novamente", {type: 'danger'});
             console.log('Error.message: ', error.message);
             console.log("error.config: ", error.config);
         }else{
